e3948fc0fcfdd4b7f991d8b16d7afd0792c33079
Андрей Богданов <cesaress@yandex.ru>
2022-10-16 20:53:48 +0300

    tasks

---
+++ element-search/menu/task.js
@@ -0,0 +1,37 @@
+const listLink = document.querySelectorAll('.menu__link');
+
+let arrLink = Array.from(listLink);
+let activeNumber = 0;
+
+for (let i = 0; i < arrLink.length; i++) {
+
+    let active = false;
+    
+    arrLink[i].onclick = function() {
+
+        let popUpMenu = arrLink[i].closest('.menu__item').querySelector('.menu_sub');
+        let popUpMenuStart = arrLink[activeNumber].closest('.menu__item').querySelector('.menu_active');
+
+        if ((popUpMenuStart != null) && (i != activeNumber)) {
+
+            popUpMenuStart.classList.remove('menu_active');
+            active = false;
+
+        };
+
+        if (!active) {
+
+            popUpMenu.classList.add('menu_active');
+            active = true;
+            activeNumber = i;
+
+        } else {
+
+            popUpMenu.classList.remove('menu_active');
+            active = false;      
+                  
+        }
+        
+        return false;
+    }
+}
---
+++ element-search/slider/task.js
@@ -0,0 +1,72 @@
+const sliderImage = document.querySelectorAll('.slider__item');
+const btnNext = document.querySelector('.slider__arrow_next');
+const btnPrev = document.querySelector('.slider__arrow_prev');
+const dotSlider = document.querySelectorAll('.slider__dot');
+
+let activeSlideIndex = 0;
+
+let arrImage = Array.from(sliderImage);
+let arrDot = Array.from(dotSlider);
+
+function addClass(number) {
+
+    arrImage[number].classList.add('slider__item_active');
+    arrDot[number].classList.add('slider__dot_active');
+
+};
+
+function removeClass(number) {
+
+    arrImage[number].classList.remove('slider__item_active');
+    arrDot[number].classList.remove('slider__dot_active');
+
+}
+
+addClass(activeSlideIndex);
+
+btnNext.onclick = function() {
+
+    removeClass(activeSlideIndex);
+
+    if (activeSlideIndex < (arrImage.length - 1)) {
+
+        ++activeSlideIndex;
+
+    } else {
+
+        activeSlideIndex = 0;
+
+    }
+
+    addClass(activeSlideIndex);
+
+}
+
+btnPrev.onclick = function() {
+
+    removeClass(activeSlideIndex);
+
+    if (activeSlideIndex > 0) {
+
+        --activeSlideIndex;
+
+    } else {
+        
+        activeSlideIndex = arrImage.length - 1;
+    }
+
+    addClass(activeSlideIndex);
+
+}
+
+for (let i = 0; i < dotSlider.length; i++) {
+
+    arrDot[i].onclick = function() {     
+
+            removeClass(activeSlideIndex);
+            addClass(i);
+            activeSlideIndex = i;
+
+    }
+
+}
